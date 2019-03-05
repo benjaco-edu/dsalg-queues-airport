4f41b8690c6718412a5f270449b98bc1cc3d5850
benjamin <cph-bl135@cphbusiness.dk>
2019-03-05 21:12:15 +0100

    pq min

---
+++ src/main/java/dk/cphbusiness/PQMin.java
@@ -0,0 +1,54 @@
+package dk.cphbusiness;
+
+public class PQMin <K extends Comparable> {
+
+    FlexibleArray<K> pq = new FlexibleArray<K>(32);
+    int head = 0;
+
+
+    public void insert(K item) {
+        pq.set(++head, item);
+        swim(head);
+    }
+
+    public K popMin() {
+        K min = pq.get(1);
+
+        swap(1, head--);
+        pq.set(head + 1, null);
+        sink(1);
+
+        return min;
+    }
+
+
+    private void swim(int index){
+        while (index > 1 && bigger(pq.get(index/2), pq.get(index))) { // pq index / 2 bigger end pg index
+            swap(index, index / 2);
+            index /= 2;
+        }
+    }
+
+    private void sink(int index){
+        while (2*index <= head) {
+            int firstOrBiggest = index * 2;
+
+            // hvis firstOrBiggest er mindre end head og firstOrBiggest er større end secound firstOrBiggest, lav beregning på secound
+            if( firstOrBiggest < head && bigger(pq.get(firstOrBiggest), pq.get(firstOrBiggest+1)) ) firstOrBiggest ++;
+            if( bigger(pq.get(firstOrBiggest), pq.get(index)) ) break;
+
+            swap(index, firstOrBiggest);
+            index = firstOrBiggest;
+        }
+    }
+
+    private void swap(int a, int b){
+        K tmp = pq.get(a);
+        pq.set(a, pq.get(b));
+        pq.set(b, tmp);
+    }
+
+    public boolean bigger(K a, K b){
+        return a.compareTo(b) > 0;
+    }
+}
